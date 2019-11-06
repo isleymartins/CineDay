f00c1fad8d92fa0f2d17dc98ad969a537cdb20b1
Isley Martins Barbosa da Silva <50592513+isleymartins@users.noreply.github.com>
2019-11-06 10:19:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+    
+  });
+  function Bobe(){
+    window.open('https://www.youtube.com/watch?v=Agqr8987-DA')
+  }
+  function FG(){
+    window.open('https://www.youtube.com/watch?v=jVdlxwX6A7g')
+  }
+  function PNemo(){
+    window.open('https://www.youtube.com/watch?v=4-LNlsaekIE')
+  }
+  function Scoobdoo(){
+    window.open('https://www.youtube.com/watch?v=9Jly1pbpWZk')
+  }
+  function ToyStory(){
+    window.open('https://www.youtube.com/watch?v=rNk1Wi8SvNc')
+  }
+  function SoulEater(){
+    window.open('https://www.youtube.com/watch?v=pcAigv_MQAo')
+  }
+ 
